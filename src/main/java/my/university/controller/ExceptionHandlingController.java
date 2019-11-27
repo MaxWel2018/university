a008a81e50d760345df67a933057825419e0aba9
MaxWel2018 <sPORT2014a>
2019-11-27 18:29:17 +0200

    refract

--- src/main/java/my/university/controller/ExceptionHandlingController.java
+++ src/main/java/my/university/controller/ExceptionHandlingController.java
@@ -2,13 +2,13 @@ package my.university.controller;
 
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.sql.SQLException;
 
-@Controller
+@ControllerAdvice
 public class ExceptionHandlingController {
     @ExceptionHandler(Exception.class)
     public String excepting() {
